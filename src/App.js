09e4a6234b23cebee13e209da1c66e8d124f1912
Sultonbek Zhoroev <85235911+sultonbekzhoroev@users.noreply.github.com>
2022-02-06 21:27:46 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./styles.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: this.props.counter
+    };
+  }
+
+  incCounter = () => {
+    if (this.state.counter < 50) {
+      this.setState((state) => ({
+        counter: state.counter + 1
+      }));
+    }
+  };
+  decCounter = () => {
+    if (this.state.counter > -50) {
+      this.setState((state) => ({
+        counter: state.counter - 1
+      }));
+    }
+  };
+
+  rndCounter = () => {
+    this.setState({
+      counter: +(Math.random() * (50 - -50) + -50).toFixed(0)
+    });
+  };
+
+  resetCounter = () => {
+    this.setState({
+      counter: this.props.counter
+    });
+  };
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <div className="app">
+        <div className="counter">{counter}</div>
+        <div className="controls">
+          <button onClick={this.incCounter}>INC</button>
+          <button onClick={this.decCounter}>DEC</button>
+          <button onClick={this.rndCounter}>RND</button>
+          <button onClick={this.resetCounter}>RESET</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
